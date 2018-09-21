a065468e1c39f8d9b339f4462470fb6d2641b272
Youjun Jin <1600991@stu.neu.edu.cn>
2018-09-21 14:15:56 +0800

    ****

--- python/interface.py
+++ python/interface.py
@@ -18,7 +18,7 @@ from time import sleep
 from tkinter import IntVar, StringVar
 
 import pymssql
-
+import math
 
 class MyGui:
     def __init__(self, framesdir, annsdir, mark_cls,drop):
@@ -364,28 +364,35 @@ class MyGui:
             # but_add.place(x=1780, y=50 + 35 * (self.num), width=50, height=30)
             # self.idlist.append(but_add)
 
-
-def mvtxt(txtdir):
+DEVICE = ['设备1']
+ZKDATE = {'_1EF40468_1531197520':'2018.7.3',
+          '_1E7ABC08_1531197647':'2018.7.4',
+          '_1EF585F0_1531203342':'2018.7.5',
+          '_1E9D6B10_1531210045':'2018.7.6',
+          '_17BA01E0_1531210125':'2018.7.7'}
+def mvtxt(analys_out):
     cnt = 0
     c = 0
     cc = 0
 
-    for t in os.listdir(txtdir):
-        if os.path.isfile(txtdir + t):
+    for t in os.listdir(analys_out):
+        if os.path.isfile(analys_out + t):
             cnt += 1
+            if not os.path.exists(analys_out + t[:t.rfind('_')]):
+                os.mkdir(analys_out + t[:t.rfind('_')])
             if t[t.rfind('_'):][1] == 'b':
                 c += 1
-                shutil.copyfile(txtdir + t, txtdir + t[:t.rfind('_')] + '/' + t)
+                os.rename(analys_out + t, analys_out + t[:t.rfind('_')] + '/' + t)
             else:
                 cc += 1
-                shutil.copyfile(txtdir + t, txtdir + t[:t.rfind('.')] + '/' + t)
+                os.rename(analys_out + t, analys_out + t[:t.rfind('.')] + '/' + t)
 
 
-def ch_time(txtdir):
-    for tn in os.listdir(txtdir):
-        if os.path.isdir(txtdir + tn):
+def ch_time(analys_out):
+    for tn in os.listdir(analys_out):
+        if os.path.isdir(analys_out + tn):
             lines = []
-            with open(txtdir + tn + '/' + tn + '.txt', 'r') as f:
+            with open(analys_out + tn + '/' + tn + '.txt', 'r') as f:
                 lines = f.readlines()
 
             newlines = []
@@ -399,30 +406,30 @@ def ch_time(txtdir):
                 line = line + ' ' + str(s) + '\n'
                 newlines.append(line)
 
-            with open(txtdir + tn + '/' + tn + '_changetime.txt', 'w') as f:
+            with open(analys_out + tn + '/' + tn + '_changetime.txt', 'w') as f:
                 f.writelines(newlines)
 
 
-def cut(videodir, chtdir):
+def cut(videodir, analys_out):
+    # videodir是原视频所在路径，analys_out是分析结果把时间从帧数换算为秒数的文本文件路径
     cnt = 0
-
     flag = 0
 
     for vn in os.listdir(videodir):
-        name = vn[:vn.rfind('.')]
-        for ds in os.listdir(chtdir + name + '/'):
-            if ds[:ds.rfind('_')] == 'cuted':
+        name = vn[:vn.rfind('.')]       # 找出mp4文件的名字
+        for ds in os.listdir(analys_out + name + '/'):
+            if ds[:ds.rfind('_')] == 'cuted':       # 对应名字的文件夹下如果有已经切过的
                 flag = 1
                 break
 
-        if flag == 1:
+        if flag == 1:                                  # 就跳过
             cnt += 1
             flag = 0
             continue
 
-        clip = VideoFileClip(videodir + vn)
+        clip = VideoFileClip(videodir + vn)             # clip是剪辑类对象
         lines = []
-        with open(chtdir + name + '/' + name + '_changetime.txt', 'r') as f:
+        with open(analys_out + name + '/' + name + '_changetime.txt', 'r') as f:
             lines = f.readlines()
 
         i = 0
@@ -431,16 +438,187 @@ def cut(videodir, chtdir):
             line = line.strip().split(' ')
             s = int(line[0])
             e = int(line[1])
-            c = clip.subclip(s, e)
-
-            c.to_videofile(chtdir + name + "/cuted_" + str(i) + ".mp4", fps=25, remove_temp=False)
+            c = clip.subclip(s, e)                      # clip剪辑从s到e的视频段
+            c.to_videofile(analys_out + name + "/cuted_" + str(i) + ".mp4", fps=25, remove_temp=False)
+                                                        # 保存
         clip.reader.close()
-
         cnt += 1
-        # 下面将地址和属性写进数据库
 
 
-def findbadvideo(videodir, outdir):
+# 给剪辑视频添加标注 将地址和属性写进数据库
+def init_db(db_host, db_name, db_username, db_pwd, db_charset):
+    pass
+    conn = pymssql.connect(db_host,db_name,db_username,db_pwd)
+    return conn
+
+def close_db(conn):
+    conn.close()
+
+def write_db(conn,filename,path,date,duration,device,category):
+    cur = conn.cursor()
+    cat = 0
+    if 1 in category:
+        cat |= 0b01
+    if 2 in category:
+        cat |= 0b10
+
+    cur.execute("insert into chaxun1(filename,path,date,duration,device,category) VALUES ('%s','%s','%s',%d,'%s',%d)" % (filename,path,date,duration,device,cat))
+    conn.comit()
+
+def name(analys_out, conn):
+    cnt = 0
+    count = 0
+    changename = ''
+    last = 1
+    end = 0
+    flag = 1
+    sflag = 0
+    eflag = 0
+    rflag = 0
+    dflag = 0
+    flagphone = 0
+    flagsleep = 0
+    yichang = 0
+    names = ['norm', 'lookphone ', 'sleep ']
+    start = []
+    stop = []
+    cfn = 0
+    tui = 0
+    filename = ''
+    path = ''
+    date = ''
+    duration = []
+    device = ''
+    category = set()
+
+
+    for d in os.listdir(analys_out):
+        duration = []
+        if os.path.isdir(analys_out + d):
+            tlines = []
+            with open(analys_out + d + '/' + d + '.txt', 'r') as f1:  # 读取帧数起止文件
+                tlines = f1.readlines()
+
+            lines = []
+            with open(analys_out + d + '/' + d + '_bbox.txt', 'r') as f:  # 读取每帧bbox文件
+                lines = f.readlines()
+
+            for tl in tlines:
+                if tl[0] == '\n':
+                    break
+                start.append(tl.strip().split(' ')[0])
+                stop.append(tl.strip().split(' ')[1])
+                duration.append(int(tl.strip().split(' ')[1]) - int(tl.strip().split(' ')[0]))
+                cnt += 1
+            print(cnt)
+            # if cnt == 12 :
+            #     pass
+            cc = cnt
+            for i in range(0, cc):
+                category.clear()
+                path = ''
+                filename = ''
+                device = ''
+                date = ''
+                for line in lines:
+                    if line.find('frame') == 0:         # 这一行是帧数行
+                        if last - 1 == cnt:             # last-1 == cnt表示所有一场片段处理完了
+                            if dflag:
+                                videowriter.write(frame)
+                            videowriter.release()
+                            cap.release()
+                            write_db(conn, filename, path, date, math.ceil(duration[last-2]/25.0), device, category)
+                            dflag = 0
+                            tui = 0
+                            last = 1
+                            start = []
+                            stop = []
+                            cnt = 0
+                            break
+
+                        line = line.strip().split(':')
+                        if line[1] == start[last - 1]:
+                            sflag = 1
+                        if line[1] == stop[last - 1]:
+                            eflag = 1
+                        if sflag:
+                            rflag = 1
+                        if dflag:
+                            # cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_CUBIC)
+                            # param = []
+                            # param.append(1)
+                            # cv2.imwrite("d:/phone/test/ss_" + str(cfn) + ".jpg", frame, param)
+                            videowriter.write(frame)
+                            dflag = 0
+                            if sflag == 0:
+                                videowriter.release()
+                                cap.release()
+                                write_db(conn, filename, path, date, math.ceil(duration[last - 2] / 25.0), device,
+                                         category)
+                                tui = 0
+                                rflag = 0
+                                break
+                            else:
+                                pass
+                        elif tui:
+                            videowriter.release()
+                            cap.release()
+                            write_db(conn, filename, path, date, math.ceil(duration[last-2]/25.0), device, category)
+                            tui = 0
+                            rflag = 0
+                            break
+                        if (flag):
+                            print(d, last)
+                            cap = cv2.VideoCapture(analys_out + d + '/' + 'cuted_' + str(last) + '.mp4')
+                            fps = 25
+                            size = (1920, 1080)
+                            filename = 'bbox_result_' + str(last) + '.mp4'
+                            # 以后开发根据原视频名判断属于哪个DEVICE的功能
+                            # 以后开发根据原视频名判断日期
+                            device = DEVICE[0]
+                            date = ZKDATE[d[d.find('_'):]]
+                            path = analys_out + d + '/' + filename
+                            videowriter = cv2.VideoWriter(path, cv2.VideoWriter_fourcc('D', 'I', 'V', 'X'), fps, size)
+                            flag = 0
+                        if eflag:
+                            tui = 1
+                            sflag = 0
+                            eflag = 0
+                            last += 1
+                            flag = 1
+                    else:
+                        if rflag:
+                            success, frame = cap.read()
+                            cfn += 1
+                            # print(type(frame))
+                            # cv2.imshow('sfd', frame)
+                            # cv2.waitKey(10000)
+                            rflag = 0
+                            dflag = 1
+
+                        line = line.strip().split(' ')
+                        cls = int(line[0])
+                        category.add(cls)
+                        x = float(line[1]) * 1920
+                        y = float(line[2]) * 1080
+                        w = float(line[3]) * 1920
+                        h = float(line[4]) * 1080
+                        x1 = int(x - 0.5 * w)
+                        y1 = int(y - 0.5 * h)
+                        x2 = int(x + 0.5 * w)
+                        y2 = int(y + 0.5 * h)
+                        if dflag:
+                            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
+                            cv2.rectangle(frame, (x1, y1 - 30), (x1 + 150, y1), (255, 0, 0), -1)
+                            cv2.putText(frame, names[cls], (x1, y1 - 10), cv2.FONT_HERSHEY_COMPLEX, 0.75, (0, 0, 0), 2)
+                            # cv2.imshow("sdf",frame)
+                            # cv2.waitKey(0)
+
+        # break
+
+
+
+def findbadvideo(videodir, outdir, ffprobe_path):
     sum = 0
     for d in os.listdir(videodir):
         if ('7号' in d):
@@ -448,7 +626,7 @@ def findbadvideo(videodir, outdir):
                 if ('mp4' in each_video):
                     filename = videodir + d + '/' + each_video
                     # filename = "d:/phone/python/7号高炉中控室_1E7ABC08_1531197647_52.mp4"
-                    result = subprocess.Popen(["D:/搜狗高速下载/ffmpeg-4.0.1-win64-static/bin/ffprobe", filename],
+                    result = subprocess.Popen([ffprobe_path, filename],
                                               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                     for x in result.stdout.readlines():
                         x = str(x, 'utf-8')
@@ -577,8 +755,7 @@ def change_key(framesdir, chkey):
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('-cls', default='cut', help='cls of interface')
-    parser.add_argument('-txtdir', default='\\path\\to\\txtdir',
-                        help='path to txtdir\n')
+    parser.add_argument('-analys_out', default='', help='path to analys_out\n')
     parser.add_argument('-videodir', default='\\path\\to\\videodir',
                         help='path to videodir\n')
     parser.add_argument('-repair_out', default='\\path\\to\\repairoutdir',
@@ -593,16 +770,24 @@ def main(argv):
     parser.add_argument('-mark_cls', default='', help='mark cls: zhongkong zhaxian\n')
     parser.add_argument('-key', default='', help='key of mp4 name\n')
     parser.add_argument('-drop', default='', help='drop out dir\n')
+    parser.add_argument('-db_username', default='', help='username\n')
+    parser.add_argument('-db_pwd', default='', help='pwd\n')
+    parser.add_argument('-db_host', default='', help='db host\n')
+    parser.add_argument('-db_name', default='', help='db name\n')
+    parser.add_argument('-db_charset', default='', help='db charset\n')
+    parser.add_argument('-ffprobe_path', default='', help='path to ffprobe\n')
 
     args = parser.parse_args()
     # 剪辑视频
     if (args.cls == "cut"):
-        mvtxt(args.txtdir)
-        ch_time(args.txtdir)
-        # 删除原txt文件
-        cut(args.videodir, args.txtdir)
+        mvtxt(args.analys_out)
+        ch_time(args.analys_out)
+        cut(args.videodir, args.analys_out)
+        conn = init_db(args.db_host, args.db_name, args.db_username, args.db_pwd, args.db_charset)
+        name(args.analys_out, conn)
+        close_db(conn)
     elif (args.cls == "repair"):  # 修复视频
-        findbadvideo(args.videodir, args.repair_out)
+        findbadvideo(args.videodir, args.repair_out, args.ffprobe_path)
         repairvideo(args.repair_out, args.videodir)
     elif (args.cls == "cap_video"):  # 获取视频帧
         cap_video(args.videodir, args.cap_out, args.cap_exe, args.cap_name)
